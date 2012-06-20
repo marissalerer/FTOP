83113992755b667f74189eb5da8382f37f56fb24
Marissa Lerer <marissalerer@gmail.com>
2012-06-19 22:33:10 -0400

    stuff

--- db/migrate/20120415231220_create_time_entries.rb
+++ db/migrate/20120415231220_create_time_entries.rb
@@ -1,7 +1,6 @@
 class CreateTimeEntries < ActiveRecord::Migration
   def change
     create_table :time_entries do |t|
-      t.string :email_address, :null => false
       t.date :date_worked, :null => false
       t.float :hours_worked, :null => false
       t.integer :coop_id, :null => false
