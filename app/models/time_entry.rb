a104f6c6b10d3546a912f4e446b2f6fa3e5d70dc
Marissa Lerer <marissalerer@gmail.com>
2012-06-19 22:25:19 -0400

    migrations

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -3,9 +3,6 @@ class TimeEntry < ActiveRecord::Base
   validates_presence_of :coop_id, 
   											:date_worked,
   											:hours_worked
-  validates_length_of :description,
-  										:comment,
-  									 	:in => 0..255
   belongs_to :member, :foreign_key => :coop_id
   validates_inclusion_of :coop_id, :in => Proc.new { Member.all.collect(&:coop_id) }
 end
