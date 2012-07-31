8a4f48ae5873093dbd26bff23ae5e54b3fd2922f
marissalerer <marissalerer@gmail.com>
2012-07-31 16:06:46 -0400

    imporving ux

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -45,7 +45,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.save
-        format.html { redirect_to @member, notice: 'Member was successfully created.' }
+        format.html { redirect_to '/members', notice: 'Member was successfully created.' }
         format.json { render json: @member, status: :created, location: @member }
       else
         format.html { render action: "new" }
@@ -61,7 +61,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.update_attributes(params[:member])
-        format.html { redirect_to @member, notice: 'Member was successfully updated.' }
+        format.html { redirect_to '/members', notice: 'Member was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -77,7 +77,7 @@ class MembersController < ApplicationController
     @member.destroy
 
     respond_to do |format|
-      format.html { redirect_to members_url }
+      format.html { redirect_to '/members', notice: 'Member was successfully deleted.' }
       format.json { head :no_content }
     end
   end
