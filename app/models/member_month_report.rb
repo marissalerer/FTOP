20818456d6b0b853226313cbbc56bfc4f798f12c
Alia Connor <aliarose@gmail.com>
2012-09-15 01:12:51 -0400

    fix up member report logic

--- app/models/member_month_report.rb
+++ app/models/member_month_report.rb
@@ -1,6 +1,55 @@
 class MemberMonthReport < ActiveRecord::Base
-  attr_accessible :carryover_hours, :member_id, :month, :shifts_worked, :year
-  
   belongs_to :member,
              inverse_of: :member_month_reports
+  
+  before_save :update_hours, if: :hours_worked_changed?
+  after_save :update_next_report, if: :carryover_hours_changed?
+  
+  def update_hours
+    last_carryover_hours = previous_report.try(:carryover_hours) || 0
+    total_hours = hours_worked + last_carryover_hours
+    self.shifts_worked = (total_hours / 2.75).floor
+    self.carryover_hours = total_hours - shifts_worked * 2.75
+  end
+  
+  def update_next_report
+    r = next_report
+    return unless r
+    
+    r.update_hours
+    r.save!
+  end
+  
+  def previous_report
+    MemberMonthReport.where('end_date <= ?', start_date).reorder(:start_date).last
+  end
+  def next_report
+    MemberMonthReport.where('start_date >= ?', end_date).reorder(:start_date).first
+  end
+  
+  
+  def self.for_date(date)
+    where("start_date <= ? AND ? < end_date", date, date).first_or_create! do |r|
+      range = range_around(date)
+      r.start_date = range.begin
+      r.end_date = range.end
+    end
+  end
+  
+  def self.range_around(date)
+    if date.day >= 15
+      start_date = date - (date.day - 15).days
+
+      end_date = date.next_month
+      end_date -= (end_date.day - 15).days
+    
+    else
+      start_date = date.prev_month
+      start_date += (15 - start_date.day).days
+  
+      end_date = date + (15 - date.day).days
+    end
+    
+    start_date...end_date
+  end
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -9,6 +9,36 @@ class TimeEntry < ActiveRecord::Base
   											:hours_worked,
   											:coop_id
 	
+
+  # Callbacks
+  after_save :update_member_month_report
+  before_destroy :remove_member_month_report_hours
+  
+  def update_member_month_report
+    old_report = member.member_month_reports.for_date(date_worked_was) if date_worked_was
+    new_report = member.member_month_reports.for_date(date_worked)
+    
+    if old_report && old_report != new_report
+      # if the reports changed
+      old_report.hours_worked -= hours_worked_was
+      new_report.hours_worked += hours_worked
+      
+      [old_report, new_report].each(&:save!)
+
+    else
+      # update the hours
+      new_report.hours_worked += hours_worked - (hours_worked_was || 0)
+      new_report.save!
+    end    
+  end
+  
+  def remove_member_month_report_hours
+    report = member.month_reports.for_date(date_worked_was)
+    report -= hours_worked_was
+    report.save!
+  end
+
+  # Virtual Attribute
   def coop_id=(id)
     self.member = Member.find_by_coop_id(id)
   end
